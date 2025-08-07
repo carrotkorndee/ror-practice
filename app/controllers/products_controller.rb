b506c284726330d62390058b6c3eb9a11f42c3a0
Carrot <carrot@odds.team>
2025-08-07 11:00:47 +0700

    Editing Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update ]
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
   end
 
   def new
@@ -14,13 +15,30 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to @product, notice: "Product was successfully created."
+      redirect_to @product
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+    # @product = Product.find(params[:id])
+  end
+
+  def update
+    # @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
   def product_params
     params.expect(product: [ :name ])
   end
